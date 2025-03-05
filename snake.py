9985476ed56225dc6f4445d3ac975343b9f8e4f8
itsarbab0 <kareemarbab00000@gmail.com>
2025-03-05 22:38:02 +0500

    First part

---
+++ snake.py
@@ -0,0 +1,57 @@
+import sys
+import pygame
+import numpy as np
+
+pygame.init()
+###  Defining Colors
+White = (255, 255, 255)
+Grey = (180, 180, 180)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Black = (0, 0, 0)
+###  Defining GAme Board
+
+Width = 300
+Height = 300
+Line_Width = 5
+Board_Col = 3
+Board_Rows = 3
+Sq_Size = Width // Board_Col
+Circle_Rad = Sq_Size // 3
+Circle_Width = 15
+Cross_Width = 25
+
+
+### Defining Screen
+Screen = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption(" Tic Tac Toe AI")
+Screen.fill(Black)
+
+### Creating Borad
+Board = np.zeros((Board_Rows, Board_Col))
+
+### Defining Functions
+def Boardlines(color = White):
+    for i in range(1, Board_Rows):
+        pygame.draw.line(Screen, color, (0, Sq_Size * i), (Width, Sq_Size * i), Line_Width) # Horizontal Lines
+        pygame.draw.line(Screen, color, (Sq_Size * i, 0), (Sq_Size * i, Height), Line_Width) # Vertical Lines
+
+def draw_Board(color = White):
+    for row in range(Board_Rows):
+        for col in range(Board_Col):
+            if Board[row][col] == 1:
+                pygame.draw.circle(Screen, color, (int(col * Sq_Size + Sq_Size // 2), int(row * Sq_Size + Sq_Size // 2)), Circle_Rad, Circle_Width)
+            elif Board[row][col] == 2:
+                pygame.draw.line(Screen, color, (col * Sq_Size + Sq_Size // 4, row * Sq_Size + Sq_Size // 4), (col * Sq_Size + 3 * Sq_Size // 4, row * Sq_Size + 3 * Sq_Size // 4))
+                pygame.draw.line(Screen, color, (col * Sq_Size + Sq_Size // 4, row * Sq_Size + 3 * Sq_Size // 4), (col * Sq_Size + 3 * Sq_Size // 4, row * Sq_Size + Sq_Size // 4))
+                
+
+def markSquare(row, col, player):
+    Board[row][col] = player
+
+
+def pos_Avail(row, col):
+    return Board[row][col] == 0
+
+def check_Boardfull(checkBoard = Board):
+    
