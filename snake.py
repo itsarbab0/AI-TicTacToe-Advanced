5b53032bc0c278bd80abe897c264686157ee5b22
itsarbab0 <kareemarbab00000@gmail.com>
2025-03-05 23:10:25 +0500

    50 persent

--- snake.py
+++ snake.py
@@ -45,13 +45,39 @@ def draw_Board(color = White):
                 pygame.draw.line(Screen, color, (col * Sq_Size + Sq_Size // 4, row * Sq_Size + Sq_Size // 4), (col * Sq_Size + 3 * Sq_Size // 4, row * Sq_Size + 3 * Sq_Size // 4))
                 pygame.draw.line(Screen, color, (col * Sq_Size + Sq_Size // 4, row * Sq_Size + 3 * Sq_Size // 4), (col * Sq_Size + 3 * Sq_Size // 4, row * Sq_Size + Sq_Size // 4))
                 
-
+###  Marking Squares
 def markSquare(row, col, player):
     Board[row][col] = player
 
-
+###   Checkingh availability of the position
 def pos_Avail(row, col):
     return Board[row][col] == 0
 
+###  Checking if the board is full
 def check_Boardfull(checkBoard = Board):
+    for row in range(Board_Rows):
+        for col in range(Board_Col):
+            if checkBoard[row][col] == 0:
+                return True
+            
+    return False 
+### Check the Winning Condition
+def chech_Win(player, checkBoard = Board):
+    for col in range(Board_Col):
+        if checkBoard[0][col] == player and checkBoard[1][col] == player and checkBoard[2][col] == player: 
+            return True
+        
+    for row in range(Board_Rows):
+        if checkBoard[row][0] == player and checkBoard[row][0] == player and checkBoard[row][0] == player: 
+            return True
+        
+    if checkBoard[0][0] == player and checkBoard[1][1] == player and checkBoard[2][2] == player:
+        return True
+    if checkBoard[0][2] == player and checkBoard[1][1] == player and checkBoard[2][0] == player:
+        return True
     
+    return False
+
+###  MiniMax The Main Function for the Game Which is basically the Whole Backbone of the Game
+def miniMax(minMaxBoard, depth, maxiMizing):
+     
